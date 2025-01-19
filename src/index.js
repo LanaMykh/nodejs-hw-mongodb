103b9448b5fb0121197c3f2eb52753de23c707f5
Svitlana Mykhailiuk <svetiklan@gmail.com>
2025-01-19 06:09:02 +0200

    step 3

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import { setupServer } from './server.js';
+import { initMongoConnection } from "./db/initMongoConnection.js";
 
-setupServer();
+const boostrap = async () => {
+    await initMongoConnection();
+    setupServer();
+};
+
+boostrap();
